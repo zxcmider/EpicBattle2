483903aeee17974ba5a4f69594abd6bd21ed7a26
Opilane TTHK <opilane@tthk.ee>
2022-01-11 13:47:29 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace EpicBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string rootPath = @"C:\Users\opilane\samples\";
+            string[] heroes = GetDataFromFile(rootPath + "heroes.txt");
+            string[] villains = GetDataFromFile(rootPath + "villains.txt");
+
+
+            string randomHero = GetRandomCharacter(heroes);
+            string randomVillains = GetRandomCharacter(villains);
+            Console.WriteLine($"Your random hero is {randomHero}");
+            Console.WriteLine($"Your random villain is {randomVillains}");
+
+            string randomWeapon = GetWeapon();
+            string randomWeapons = GetWeapon();
+            Console.WriteLine($"Your weapon Hero {randomWeapon}");
+            Console.WriteLine($"Your weapon Villain {randomWeapons}");
+        }
+
+        public static string GetRandomCharacter(string[] someArray)
+        {
+
+            return someArray[GetRandomIndexForArray(someArray)];
+        }
+
+        public static string GetWeapon()
+        {
+            string[] weapon = { "AWP", "Desert Eagle", "AK-47", "Knife", "Pen" };
+            return weapon[GetRandomIndexForArray(weapon)];
+        }
+        public static int GetRandomIndexForArray(string[] someArray)
+        {
+            Random rnd = new Random();
+            return rnd.Next(0, someArray.Length);
+        }
+
+        public static string[] GetDataFromFile(string filePath)
+        {
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            return dataFromFile;
+        }
+
+    
+    }
+}
